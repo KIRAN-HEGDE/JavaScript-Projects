bf90cf207df42d77042346f3ef14d59402370fad
Kiran Hegde <50022409+KIRAN-HEGDE@users.noreply.github.com>
2020-06-11 16:55:02 +0530

    created the color picker project

---
+++ Color-Picker/app.js
@@ -0,0 +1,15 @@
+const colors= [ "aqua","black" , "blue", "fuchsia", "gray", "grey", "green", "lime", "maroon", "navy", "olive", "purple", "red", "silver", "teal", "white", "yellow"];
+const btn = document.getElementById('btn');
+const printer = document.getElementById('bg-color-printer');
+
+function getRandomIndex(){
+    return Math.floor(Math.random() * colors.length);
+}
+console.log(getRandomIndex());
+console.log(btn);
+
+btn.addEventListener("click", function(){
+    index = getRandomIndex();
+    document.body.style.backgroundColor = colors[index];
+    printer.innerHTML = colors[index];
+});
---
+++ Color-Picker/hex.js
@@ -0,0 +1,21 @@
+const hex_vars = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const btn = document.getElementById('btn');
+const printer = document.getElementById('bg-color-printer');
+
+
+function getHex() {
+    var hexColor = "#";
+    for(var i = 0; i < 6; i++){
+        index = Math.floor(Math.random() * hex_vars.length)
+        hexColor += String(hex_vars[index]);
+    }
+    return hexColor;
+}
+
+
+btn.addEventListener("click", function(){
+    var color = getHex();
+    console.log(color);
+    document.body.style.backgroundColor = color;
+    printer.innerHTML = color;
+});
